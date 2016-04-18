20c2f321be6ef1a58d07b10f5f4444f248bdcdae
Florent Sarembaud <cromod@users.noreply.github.com>
2016-04-18 19:35:02 +0200

    Improve documentation

--- PyBF.py
+++ PyBF.py
@@ -5,41 +5,44 @@ import sys
 byte_array = [0] # array of bytes
 data_ptr = 0 # data pointer
 
-# Increment the byte at the data pointer
+# Basic instructions except loop
 def increment_byte():
+    """Increment the byte at the data pointer."""
     byte_array[data_ptr] += 1
 
-# Decrement the byte at the data pointer
 def decrement_byte():
+    """Decrement the byte at the data pointer."""
     byte_array[data_ptr] -= 1
-    #if byte_array[data_ptr] < 0:
-        #raise ValueError("Negative value in array")
 
-# Increment the data pointer
 def increment_ptr():
+    """Increment the data pointer."""
     global data_ptr
     data_ptr += 1
     if data_ptr == len(byte_array):
         byte_array.append(0)
 
-# Decrement the data pointer
 def decrement_ptr():
+    """Decrement the data pointer.
+
+    Raises:
+        ValueError: if pointer is negative
+    """
     global data_ptr
     data_ptr -= 1
     if data_ptr < 0:
         raise ValueError("Negative value of pointer")
 
-# Output the byte at the data pointer
 def output():
+    """Output the byte at the data pointer."""
     sys.stdout.write(chr(byte_array[data_ptr]))
 
-# Store one byte of input in the byte at the data pointer
 def store():
+    """Output the byte at the data pointer."""
     save = sys.stdin.read(1)
     if save:
         byte_array[data_ptr] = ord(save)
 
-# Read instructions except loop
+# Dictionary to call instructions except loop
 instructions = {
     '+': increment_byte,
     '-': decrement_byte,
@@ -49,14 +52,18 @@ instructions = {
     ',': store
 }
 
-# Interpret brainfuck code
+# Interpreter
 def interpret(char_chain):
+    """Interpret brainfuck code."""
     skip = 0
     for it, char in enumerate(char_chain):
         if skip:
-            if char == '[': skip += 1 # Increment the level of nested loops to skip
-            if char == ']': skip -= 1 # Decrement the level of nested loops to skip
-            continue # Actually skip that character
+            # Increment the level of nested loops to skip
+            if char == '[': skip += 1
+            # Decrement the level of nested loops to skip
+            if char == ']': skip -= 1
+            # Skip that character
+            continue
         if char == '[':
             while byte_array[data_ptr]:
                 # Recurse to interpret the loop
@@ -67,10 +74,13 @@ def interpret(char_chain):
             # End the recursion when reaching the end of the loop
             return
         else:
+            # Read instructions except loop
             instructions.get(char, lambda: None)()
+    # Display a warning if there is no closing bracket
     if skip > 0:
         print "\nWarning: No closing bracket"
 
+# Main
 if __name__ == "__main__":
     try:
         file_name = sys.argv[1]
